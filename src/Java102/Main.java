64e7df7a1df9e20037b323f14065729eb46d61b6
edaUstun <edaustun1141@gmail.com>
2022-09-20 11:47:07 +0300

    Encapsulation

--- src/Java102/Main.java
+++ src/Java102/Main.java
@@ -1,5 +1,6 @@
 package Java102;
 
+import Java102.Encapsulation.Book;
 import Java102.Static.User;
 
 import java.util.Scanner;
@@ -10,11 +11,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /*
+        Static Kod Blogu
+
         User u1 = new User("Mustafa");
         User u2 = new User("Patika");
         User u3 = new User("Kalamar");
 
         System.out.println(User.counter);
+
+         */
+
+        Book harryPotter = new Book("Harry Potter", 154);
+        Book lordOfTheRings = new Book("Lord Of the Rings", -100);
+        /*
+        lordOfTheRings.setName("ABC");
+
+        System.out.println(lordOfTheRings.getName());
+         */
+
+        System.out.println(lordOfTheRings.getPageNumber());
 
 
 
